fa76b9369774b555ba106a3491e06e09b49d1635
XinruiHe <hexinrui@usc.edu>
2025-02-10 18:00:42 -0800

    The appearance of the enemy WAVE and the corresponding base deducting blood
    This time I finished creating the enemy UI and the end life values. And created corresponding scripts for the enemies and endpoints. The scenario I tried in the script was to fix the game at a certain position after reaching the end point, and if the end point life value reaches 0, the game will be terminated directly and a game over UI popup will be created later. The option of destroying the enemy UI after reaching the end is not feasible because of an unknown bug that makes all enemy UI disappear when destroyed.

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public int health = 10;  // 终点生命值
+    private bool isGameOver = false; // 防止重复触发 GameOver
+
+    public void TakeDamage(int damage)
+    {
+        if (isGameOver) return; // **如果游戏已经结束，就不再扣血**
+        if (health <= 0) return;
+
+        health -= damage;
+        if (health < 0) health = 0;
+
+        //Debug.Log("EndTrigger 生命值减少，当前生命值: " + health);
+
+        if (health <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        if (isGameOver) return; // **防止重复调用**
+        isGameOver = true;
+
+        //Debug.Log("游戏失败！终点被摧毁！");
+
+        // **暂停游戏**
+        Time.timeScale = 0;
+
+        // 这里可以添加游戏失败的 UI
+        ShowGameOverScreen();
+    }
+
+    void ShowGameOverScreen()
+    {
+        // 这里可以调用你的 UI 管理器，显示一个 Game Over 界面
+        Debug.Log("显示 Game Over 界面");
+    }
+}
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,18 +1,56 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class EnemyMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 2.0f;  // 敌人移动速度
+    private Transform target;   // 目标点（终点）
+    private bool isDamaged = false; // **防止重复触发**
+
     void Start()
     {
-        
+        GameObject endTriggerObject = GameObject.Find("EndTrigger");
+        if (endTriggerObject != null)
+        {
+            target = endTriggerObject.transform;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (target != null && !isDamaged)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isDamaged) return;
+
+        if (other.gameObject.CompareTag("EndTrigger"))
+        {
+            //Debug.Log(gameObject.name + " 进入 EndTrigger，造成伤害！");
+
+            isDamaged = true;  // **防止重复扣血**
+
+            // 造成伤害
+            EndTrigger endTrigger = other.GetComponent<EndTrigger>();
+            if (endTrigger != null)
+            {
+                endTrigger.TakeDamage(1);
+            }
+
+            // **停止移动**
+            StopEnemy();
+        }
+    }
+
+    void StopEnemy()
+    {
+        //Debug.Log(gameObject.name + " 停止移动！");
+        speed = 0; // **速度归零**
+        transform.position += new Vector3(0.5f, 0, 0); // **稍微移动到终点后方**
     }
 }
