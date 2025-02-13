7daa7b0d43fbdcc1a6ac8e6cd06d27b8e3560dd2
theTY2002 <71263782+theTY2002@users.noreply.github.com>
2025-02-12 22:15:10 -0800

    Player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private bool isRepeatedMovement = false;
+    [SerializeField] private float moveDuration = 0.1f;
+    [SerializeField] private float gridSize = 1f;
+
+    private bool isMoving;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (!isMoving)
+        {
+            System.Func<KeyCode, bool> inputFunction;
+            if (isRepeatedMovement)
+            {
+                inputFunction = Input.GetKey;
+            }
+            else
+            {
+                inputFunction = Input.GetKeyDown;
+            }
+
+            if (inputFunction(KeyCode.UpArrow))
+            {
+                StartCoroutine(Move(Vector2.up));
+            }
+            else if (inputFunction(KeyCode.DownArrow))
+            {
+                StartCoroutine(Move(Vector2.down));
+            }
+            else if (inputFunction(KeyCode.LeftArrow))
+            {
+                StartCoroutine(Move(Vector2.left));
+            }
+            else if (inputFunction(KeyCode.RightArrow))
+            {
+                StartCoroutine(Move(Vector2.right));
+            }
+        }
+    }
+
+    private IEnumerator Move(Vector2 direction)
+    {
+        isMoving = true;
+
+        Vector2 startPos = transform.position;
+        Vector2 endPos = startPos + (direction * gridSize);
+
+        float elapsedTime = 0;
+        while (elapsedTime < moveDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float percent = elapsedTime / moveDuration;
+            transform.position = Vector2.Lerp(startPos, endPos, percent);
+            yield return null;
+        }
+
+        transform.position = endPos;
+
+        isMoving = false;
+    }
+}
