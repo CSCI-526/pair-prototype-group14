51e12c8e1af6f934ed138e29ea26a581e53e12ca
theTY2002 <71263782+theTY2002@users.noreply.github.com>
2025-02-20 00:50:46 -0800

    New functionality + Fixes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -47,7 +47,7 @@ public class PlayerMovement : MonoBehaviour
             // {
             //     yield return null;
             // }
-            if (movesLeft > 0)
+            if (movesLeft > 0 && GameObject.FindGameObjectWithTag("Enemy") == null)
             {
                 if (inputFunction(KeyCode.UpArrow))
                 {
